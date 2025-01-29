7af6e3cfd682d1429485d6aa86ec229a4b90823f
John Sattari <jsattar3@gmail.com>
2025-01-28 20:25:38 -0800

    made updates to fix avoids flag and to remove chars that are not needed

--- src/main.rs
+++ src/main.rs
@@ -19,39 +19,52 @@ struct Args {
 }
 
 fn main() {
-    // output
-    let mut password: Vec<u32> = Vec::new();
-
     // parse input
     let args = Args::parse();
+
+    println!("{:?}", args);
 
     // get desired len of password
     let arr_len: usize = args.length.parse().unwrap();
 
-    // create
+    // create placeholder for output
+    let mut password: Vec<u32> = vec![0; arr_len];
 
-    // if avoid, get vector of chars
-    // let avoids: Vec<usize> = args.avoid.chars().map(|c| c as usize).collect();
+    // create list of values representing usable chars
+    let mut _ascii: Vec<u32> = (33..127).map(|n| n as u32).collect();
+
+    // if avoid, create vec of avoidance chars and remove from ascii_chars_vec
+    let avoids: Vec<u32> = args.avoid.chars().map(|c| c as u32).collect();
+
+    for i in avoids.iter() {
+        if let Some(position) = _ascii.iter().position(|x| x == i) {
+            _ascii.remove(position);
+        }
+    }
+
+    println!("{:?}", _ascii);
 
     // append n chars to vector based on length flag
-    for _ in 0..arr_len {
+    for idx in 0..arr_len {
         // get random value
-        let mut rando: u32 = choose_rand(33, 128) as u32;
+        let rando: u32 = choose_rand(0, _ascii.len() as i32) as u32;
 
         // check if repeats
-        if args.repeats && password.contains(&rando) {
-            rando = (rando + 33) % 95
+        if args.repeats {
+            if let Some(position) = _ascii.iter().position(|&x| x == rando) {
+                _ascii.remove(position);
+            }
         }
 
-        // create ascii char
-        // let _ascii: char = std::char::from_u32(rando as u32).unwrap();
-
         // append to output vector
-        password.push(rando);
+        password[idx] = _ascii[rando as usize];
+
+        println!("{:?}", password);
     }
 
     // turn into string and return password
     let password_str: String = password.iter().filter_map(|&n| char::from_u32(n)).collect();
+
     println!("{}", password_str)
 }
 
@@ -81,12 +94,4 @@ fn choose_rand(min: i32, max: i32) -> i32 {
     let result = rand_val % range;
 
     min + result
-}
-
-fn avoid_chars(ord: u32, bad_chars: String) -> u32 {
-    // turn input into a char
-    let temp_char: char = std::char::from_u32(rando as u32).unwrap();
-
-    // check if char is avoid list
-    if bad_chars.contains(temp_char) {}
 }
