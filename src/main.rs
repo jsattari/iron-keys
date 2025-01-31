9d35848c8fb161dd3917d999bd9a74daa4f1a082
John Sattari <jsattar3@gmail.com>
2025-01-30 23:37:17 -0800

    updated ascii chars to be a hashmap instead of vector; modularized password creation; updated secondary functions so they can accept a inputs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
-use std::process::Command;
+mod utils;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
-/// create  passwords
+/// Create passwords
 struct Args {
     #[arg(short('l'), long, default_value = "8", required(false))]
     // Desired length of password
@@ -19,79 +19,21 @@ struct Args {
 }
 
 fn main() {
-    // parse input
+    // parse input arguments
     let args = Args::parse();
 
-    println!("{:?}", args);
+    // get desired password length, handle potential parsing errors
+    let password_length: usize = args.length.parse().unwrap_or_else(|_| {
+        eprintln!("Invalid length value: {}", args.length);
+        std::process::exit(1);
+    });
 
-    // get desired len of password
-    let arr_len: usize = args.length.parse().unwrap();
+    // Create list of usable ASCII characters
+    let mut ascii_chars = utils::generate_ascii_chars(&args.avoid);
 
-    // create placeholder for output
-    let mut password: Vec<u32> = vec![0; arr_len];
+    // create output
+    let password = utils::generate_password(password_length, &mut ascii_chars, args.repeats);
 
-    // create list of values representing usable chars
-    let mut _ascii: Vec<u32> = (33..127).map(|n| n as u32).collect();
-
-    // if avoid, create vec of avoidance chars and remove from ascii_chars_vec
-    let avoids: Vec<u32> = args.avoid.chars().map(|c| c as u32).collect();
-
-    for i in avoids.iter() {
-        if let Some(position) = _ascii.iter().position(|x| x == i) {
-            _ascii.remove(position);
-        }
-    }
-
-    println!("{:?}", _ascii);
-
-    // append n chars to vector based on length flag
-    for idx in 0..arr_len {
-        // get random value
-        let rando: u32 = choose_rand(0, _ascii.len() as i32) as u32;
-
-        // check if repeats
-        if args.repeats {
-            if let Some(position) = _ascii.iter().position(|&x| x == rando) {
-                _ascii.remove(position);
-            }
-        }
-
-        // append to output vector
-        password[idx] = _ascii[rando as usize];
-
-        println!("{:?}", password);
-    }
-
-    // turn into string and return password
-    let password_str: String = password.iter().filter_map(|&n| char::from_u32(n)).collect();
-
-    println!("{}", password_str)
-}
-
-fn generate_rand_byte(count: usize) -> Vec<u8> {
-    // use randomness source from sys
-    let output = Command::new("dd")
-        .arg("if=/dev/urandom")
-        .arg("bs=1")
-        .arg(format!("count={}", count))
-        .output()
-        .expect("failed to read random bytes");
-
-    output.stdout
-}
-
-fn choose_rand(min: i32, max: i32) -> i32 {
-    // find range
-    let range = max - min;
-
-    // get enough bytes for an i32
-    let rand_bytes = generate_rand_byte(1);
-
-    // convert rand bytes into i32 val
-    let rand_val = rand_bytes[0] as i32;
-
-    // limit val to range (0, max) using modulus
-    let result = rand_val % range;
-
-    min + result
+    // output
+    println!("{}", password);
 }
